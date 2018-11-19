da9ac609621f5d371a4d6ad5eecccd353ea19bfd
g1727010 <32998565+g1727010@users.noreply.github.com>
2018-11-19 17:57:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -69,3 +69,22 @@ update();
 document.getElementById('form').select.onchange = function(){
   location.href = document.getElementById('form').select.value;
 }
+
+function getFileName(){
+  return windows.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}
+else{
+  opt = document.querySelector('option[value="index.html"]');
+}
+
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
+}
